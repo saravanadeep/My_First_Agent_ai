b359574637941ec526f0671e2fa257049792a6f3
saravanadeep <krsaravana4321@gmail.com>
2026-02-23 22:38:51 +0530

    Set up root_agent with configuration details
    Initialized root_agent with model, name, description, and instruction.

--- AGENT_AI/agent_eco/agent.py
+++ AGENT_AI/agent_eco/agent.py
@@ -1,8 +1,18 @@
+#Importing the agent from the google adk agent kit.
 from google.adk.agents.llm_agent import Agent
 
+#Declaring imported agent to root_agent.
 root_agent = Agent(
+
+    #model name of the agent.
     model='gemini-2.5-flash',
+
+    #Name of the agent.
     name='root_agent',
+
+    #Description of the agent.
     description='A helpful assistant for user questions.',
+
+    #Giving instruction to the agent.
     instruction='Answer user questions to the best of your knowledge',
 )
